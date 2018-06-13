47159869d17e649311cfb095f14ddf856639a314
mbohnearschi <mbohnearschi@mixbook.com>
2018-06-13 15:48:48 +0300

    Popcorn tests + optimization

--- lib/popcorn.rb
+++ lib/popcorn.rb
@@ -8,7 +8,7 @@ class WordHolder
   end
 
   def build_word(letters)
-    @used_letter_ids.map { |id| letters[id] }.join
+    @used_letter_ids.map {|id| letters[id]}.join
   end
 
   def copy(used_letter_ids)
@@ -19,11 +19,12 @@ class WordHolder
 end
 
 class PopcornSolver
-  attr_accessor :letters, :connections
+  attr_accessor :letters, :connections, :test_words
 
-  def initialize(letters, connections)
+  def initialize(letters, connections, test_words)
     @letters = letters
     @connections = connections
+    @test_words = test_words
   end
 
   def words
@@ -36,13 +37,15 @@ class PopcornSolver
 
   def find_words(word_holder, current_letter_idx)
     found_words = []
-
     if word_holder.used_letter_ids.include?(current_letter_idx)
       word = word_holder.build_word(letters)
       found_words += [word] if valid_word?(word)
     else
       new_used_letter_ids = word_holder.used_letter_ids + [current_letter_idx]
       new_word_holder = word_holder.copy(new_used_letter_ids)
+
+      new_word = new_word_holder.build_word(letters)
+      return [] unless any_word_starts_with(new_word)
 
       connections[current_letter_idx].each do |connection_idx|
         found_words += find_words(new_word_holder, connection_idx)
@@ -53,6 +56,13 @@ class PopcornSolver
   end
 
   def valid_word?(word)
-    %w[POP CAT DOG ROCK POPCORN CORN].include?(word)
+    @test_words.include?(word)
+  end
+
+  def any_word_starts_with(characters)
+    @test_words.each do |test_word|
+      return true if test_word.start_with?(characters)
+    end
+    false
   end
 end
